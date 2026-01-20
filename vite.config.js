117077df729a4ec55df2b1887b3a0ef55b5ef076
Hanna Lindkvist <hali2507@student.miun.se>
2026-01-20 14:12:49 +0100

    lägger till htmlsidor + viteconfig

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import  { resolve } from "path";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                pictures: resolve(__dirname, "pictures.html"),
+                process: resolve(__dirname, "process.html")
+            }
+        }
+    }
+})
