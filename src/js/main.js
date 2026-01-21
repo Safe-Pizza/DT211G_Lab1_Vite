7c4b06725de4f11c0749ba570bef9d26af9782e6
Hanna Lindkvist <hali2507@student.miun.se>
2026-01-21 15:02:31 +0100

    skapar navigation för mobil

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 "use strict";
 
-console.log("Hello World!!!");
+document.querySelector("#hamburgerMenu").addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    var x = document.querySelector(".mainNav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
